d024497b44cda378d353912c2ee8ba7925f42265
Arno Hovhannisyan <arnohovhannisyan0@gmail.com>
2021-01-05 10:36:34 -0800

    add minotaur

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,20 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.modrinth.minotaur.TaskModrinthUpload
+
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.targetCompatibility = JavaVersion.VERSION_1_8
+
+fun getArtifactPath(): String {
+	return tasks.named<Jar>("jar").get().archiveFile.get().toString()
+}
+
+fun p(name: String): String {
+	return project.property(name).toString()
+}
 
 plugins {
 	id("fabric-loom")
+	id("com.modrinth.minotaur") version "1.1.0"
 	kotlin("jvm") version "1.4.21"
 }
 
@@ -10,11 +23,11 @@ repositories {
 }
 
 dependencies {
-	minecraft("com.mojang:minecraft:${project.property("minecraft")}")
-	mappings("net.fabricmc:yarn:${project.property("yarn")}:v2")
-	modImplementation("net.fabricmc:fabric-loader:${project.property("loader")}")
-	modImplementation("net.fabricmc.fabric-api:fabric-api:${project.property("fabric_api")}")
-	modImplementation("net.fabricmc:fabric-language-kotlin:${project.property("fabric_kotlin")}")
+	minecraft("com.mojang:minecraft:${p("minecraft.target")}")
+	mappings("net.fabricmc:yarn:${p("yarn")}:v2")
+	modImplementation("net.fabricmc:fabric-loader:${p("loader")}")
+	modImplementation("net.fabricmc.fabric-api:fabric-api:${p("fabric_api")}")
+	modImplementation("net.fabricmc:fabric-language-kotlin:${p("fabric_kotlin")}")
 }
 
 tasks.named<Copy>("processResources") {
@@ -30,10 +43,27 @@ tasks.named<Copy>("processResources") {
 	}
 }
 
-tasks.withType<JavaCompile> {
-	options.encoding = "UTF-8"
-}
-
 tasks.withType<KotlinCompile> {
 	kotlinOptions.jvmTarget = "1.8"
+}
+
+task<TaskModrinthUpload>("modrinth") {
+	token = System.getenv("MODRINTH_TOKEN")
+	projectId = p("id")
+
+	versionNumber = p("version")
+	versionName = "${project.property("name")} v$versionNumber"
+
+	uploadFile = getArtifactPath()
+
+	addGameVersion(p("minecraft.target"))
+
+	p("minecraft.compatible").split(", ").forEach { addGameVersion(it) }
+
+	addLoader("fabric")
+}
+
+task("publish") {
+	dependsOn("build")
+	dependsOn("modrinth")
 }
