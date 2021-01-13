c232540a4be6a41101f12dfb6b2d519d41250708
Arno Hovhannisyan <arnohovhannisyan0@gmail.com>
2021-01-13 15:03:57 -0800

    get tag better

--- src/main/kotlin/io/glossnyx/disco/LootTables.kt
+++ src/main/kotlin/io/glossnyx/disco/LootTables.kt
@@ -4,21 +4,15 @@ import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder
 import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback
 import net.minecraft.loot.ConstantLootTableRange
 import net.minecraft.loot.entry.TagEntry
-import net.minecraft.tag.ServerTagManagerHolder
+import net.minecraft.tag.ItemTags
 import net.minecraft.util.Identifier
 
 fun modifyLootTables() {
 	LootTableLoadingCallback.EVENT.register { _, _, id, supplier, _ ->
-		val tagManager = ServerTagManagerHolder.getTagManager()
-
-		val rolls = ConstantLootTableRange(1)
-
 		addons.filter { it.tables.contains(id) }.forEach {
-			val tag = tagManager.items.getTag(Identifier(it.id, "discs"))
-
 			val pool = FabricLootPoolBuilder.builder()
-				.rolls(rolls)
-				.withEntry(TagEntry.builder(tag).build())
+				.rolls(ConstantLootTableRange(1))
+				.with(TagEntry.builder(ItemTags.getTagGroup().getTag(Identifier(it.id, "discs"))))
 				.build()
 
 			supplier.withPool(pool)
